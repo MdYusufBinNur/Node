2c50bde91619029eb5e52e0df3409f046a037102
MdYusufBinNur <engr.mdyusuf@gmail.com>
2020-02-24 17:40:04 +0600

    Init

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const mongoose = require('mongoose')
 const app = express();
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+//Passport Config
+require('./config/passport')(passport);
+
 
 //Mongo Db
 const db = require('./config/keys').MongoURI;
@@ -17,6 +23,30 @@ app.set('view engine', 'ejs');
 
 //BodyParser
 app.use(express.urlencoded({ extend: false}));
+
+//Express-Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+
+}));
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+
+});
+
 
 //Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../Models/User');
-const bycrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/login', (req, res) => res.render('login'));
 router.get('/register', (req, res) => res.render('register'));
 router.post('/register',(req, res) => {
-    const { name, email,password, password2} = req.body;
+    const { name, email,password, password2 } = req.body;
     let errors = [];
 
     if (!name || !email || !password || !password2){
@@ -21,7 +23,6 @@ router.post('/register',(req, res) => {
             errors, name, email, password, password2
         })
     }else {
-
         User.findOne({ email: email})
             .then(user => {
                 if (user){
@@ -30,10 +31,38 @@ router.post('/register',(req, res) => {
                         errors, name, email, password, password2
                     })
                 }else {
-                    res.render('login')
+                    const newUser = new User({name, email, password});
+                    bcrypt.genSalt(10,(err, salt) =>
+                        bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                            if (err) throw err;
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg','You are registered');
+                                    res.redirect('/users/login')
+                                })
+                                .catch(err =>console.log(err))
+                        }))
+
                 }
             })
-
     }
 });
+
+// Login
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+//Logout
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    req.flash('success_msg','Successfully logged out');
+    res.redirect('/users/login')
+})
+
 module.exports = router;
